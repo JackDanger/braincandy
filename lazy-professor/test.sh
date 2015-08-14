102342e42bc5716faaa38e4757cf51bc20a00ba9
Jack Danger Canty <github@jackcanty.com>
2015-08-13 20:55:19 -0700

    testing Python/Ruby/Go/Java

--- lazy-professor/test.sh
+++ lazy-professor/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-diff <(python answers.py) <(ruby answers.rb)
+diff <(ruby answers.rb) <(python answers.py)
+diff <(ruby answers.rb) <(javac Answers.java && java Answers)
+diff <(ruby answers.rb) <(go run answers.go)
