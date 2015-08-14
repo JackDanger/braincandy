1624ffae66310f936552200de8e082a34ddf9ead
Jack Danger Canty <github@jackcanty.com>
2015-08-14 09:57:39 -0700

    Finishing test script

--- lazy-professor/test.sh
+++ lazy-professor/test.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-diff <(ruby answers.rb) <(python answers.py)
-diff <(ruby answers.rb) <(javac Answers.java && java Answers)
-diff <(ruby answers.rb) <(go run answers.go)
+run(){
+  case $1 in
+    'python')
+      python answers.py
+      ;;
+    'ruby')
+      ruby answers.rb
+      ;;
+    'clojure')
+      lein repl <<< '(load-file "answers.clj")' | grep output | cut -d ' ' -f 2
+      ;;
+    'java')
+      javac Answers.java
+      java Answers
+      ;;
+    'go')
+      go run answers.go
+      ;;
+  esac
+}
+
+ruby_out=$(run ruby)
+python_out=$(run python)
+java_out=$(run java)
+go_out=$(run go)
+clojure_out=$(run clojure)
+diff <(echo $ruby_out) <(echo $python_out)  && echo "Python: OK"
+diff <(echo $ruby_out) <(echo $java_out)    && echo "Java: OK"
+diff <(echo $ruby_out) <(echo $go_out)      && echo "Go: OK"
+diff <(echo $ruby_out) <(echo $clojure_out) && echo "Clojure: OK"
