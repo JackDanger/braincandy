825826f1459f10c40a5589268f3955c717a70a95
Jack Danger Canty <github@jackcanty.com>
2015-08-16 19:44:42 -0700

    adding ZSH solution
    I think I have a problem

---
+++ lazy-professor/answers.zsh
@@ -0,0 +1,67 @@
+#!/bin/zsh
+
+typeset -A counts
+counts[A]=""
+counts[B]=""
+counts[C]=""
+counts[D]=""
+
+sample=$(head -n 1 test.txt)
+num_questions=$(wc -c <<<$sample)
+
+# initialize the count lists with double-digit numbers
+for question in $(seq $num_questions); do
+  counts[A]="00${counts[A]}"
+  counts[B]="00${counts[B]}"
+  counts[C]="00${counts[C]}"
+  counts[D]="00${counts[D]}"
+done
+
+for answer in `cat test.txt`; do
+  for question in $(seq $num_questions); do
+    # The letter chosen at the position $question
+    local choice=$(echo -n $answer | cut -c $question)
+    local digit_offset_ones=$((question * 2)) # double-digit serialization
+    local digit_offset_tens=$(($digit_offset_ones - 1))
+    local current="${counts[$choice][$digit_offset_tens]}${counts[$choice][$(($digit_offset_ones))]}"
+    local new_count=$(($current + 1))
+    local count=${counts[$choice]}
+
+    # turn single-digit numbers (which, which the heredoc, look like two-digit)
+    # into double-digit ones.
+    if [[ 2 -eq $(wc -c <<<$new_count) ]]; then
+      new_count="0$new_count"
+    fi
+
+    #if [[ "A" == "$choice" ]]; then
+    #  echo answer $answer
+    #  echo question $question
+    #  echo choice $choice
+    #  echo digit_offset_tens $digit_offset_tens
+    #  echo digit_offset_ones $digit_offset_ones
+    #  echo current $current
+    #  echo new_count $new_count
+    #  echo count $count
+    #fi
+
+    count[$digit_offset_tens]=${new_count[1]}
+    count[$digit_offset_ones]=${new_count[2]}
+    counts[$choice]=$count
+  done
+done;
+
+for question in `seq $num_questions`; do
+  local digit_offset_ones=$((question * 2)) # double-digit serialization
+  local digit_offset_tens=$(($digit_offset_ones - 1))
+  local best_choice=''
+  local best_count=0
+  for choice in A B C D; do
+    this_count="${counts[$choice][$digit_offset_tens]}${counts[$choice][$(($digit_offset_ones))]}"
+    if [[ $this_count -gt $best_count ]]; then
+      best_choice=$choice
+      best_count=$this_count
+    fi
+    echo -n $best_choice
+  done
+done
+
